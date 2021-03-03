8ea114b58a578c6f8347c8afa636c224588aad7b
agiannell <agiannell@gmail.com>
2021-03-03 16:22:13 -0700

    uncommented express.static line

--- server/index.js
+++ server/index.js
@@ -1,14 +1,9 @@
 require('dotenv').config();
 const express = require('express'),
-      path = require('path'),
       { SERVER_PORT } = process.env,
       app = express();
 
 app.use(express.json());
-// app.use(express.static(__dirname + '/../build'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
-});
+app.use(express.static(__dirname + '/../build'));
 
 app.listen(SERVER_PORT, () => console.log(`Impressing employers on port ${ SERVER_PORT }`));
